f7aa5d79b463839e240e9032c9d6a429bbc870f8
Brandon Warrior <brandonwarrior@Brandons-MBP.lan>
2025-02-09 16:38:07 +0000

    Added home page with featured posts section, intergrated with bootstrap

--- ukhikes/urls.py
+++ ukhikes/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+from blog.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    path('', home, name='home'),
+    path('blog/', home, name='blog'),
 ]
