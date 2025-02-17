498edceae9f3efbbf15762060697b8e05df46cca
Brandon Warrior <brandonwarrior@Brandons-MBP.lan>
2025-02-17 17:12:05 +0000

    update site domain and applied site migration fix

--- ukhikes/settings.py
+++ ukhikes/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security Settings
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-wwfshzz==l8_+s(99staqopy=trqws2adfz^#1nz(v@-sizhfh')
-DEBUG = True
+DEBUG = False
 
 SITE_ID = 1
 
