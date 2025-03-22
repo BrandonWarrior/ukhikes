f512ac692896be68735418aba4d58ec9dcbb3a59
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-03-22 18:42:34 +0000

    set debug to false for deployment

--- ukhikes/settings.py
+++ ukhikes/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Set the debug flag based on the environment
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = False
 
 # This is for local development; in production, set SECURE_SSL_REDIRECT to True
 SECURE_SSL_REDIRECT = False
